34ed9094117d7dea72e2587d7ad25c7f36aeb719
Ramjan Ali <ramjan.ali.ashik@gmail.com>
2024-01-14 19:18:39 +0000

    setting transaction readonly

--- src/main/java/co/uk/apg/service/impl/EventServiceImpl.java
+++ src/main/java/co/uk/apg/service/impl/EventServiceImpl.java
@@ -8,6 +8,7 @@ import co.uk.apg.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
 
